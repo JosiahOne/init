81da08d036cb8b81dff8795891519e0ec7fcad37
Josiah Bruner <jsbruner@umich.edu>
2019-04-12 20:53:58 -0400

    Add Google Calendar

--- macos-setup.sh
+++ macos-setup.sh
@@ -25,6 +25,7 @@ npm install nativefier -g;
 # appropiate.
 
 # FB Messenger
+rm -rf /Applications/Messenger.app/;
 nativefier --name "Messenger" messenger.com;
 cd Messenger*;
 cp -r Messenger.app/ /Applications/Messenger.app/;
@@ -32,9 +33,17 @@ cd ..;
 rm -rf Messenger-darwin-x64/;
 
 # Google Messages
+rm -rf /Applications/Google\ Messages.app/;
 nativefier --name "Google Messages" messages.google.com/web;
 cd Google\ Messages-darwin-x64/;
 cp -r Google\ Messages.app/ /Applications/Google\ Messages.app/
 cd ..;
 rm -rf Google\ Messages-darwin-x64/;
 
+# Google Calendar
+rm -rf /Applications/Google\ Calendar.app/; 
+nativefier --name "Google Calendar" calendar.google.com;
+cd Google\ Calendar-darwin-x64/;
+cp -r Google\ Calendar.app/ /Applications/Google\ Calendar.app/
+cd ..;
+rm -rf Google\ Calendar-darwin-x64/;
