fc0c48419f6da206c4a784b264ac8396141dfe62
Josiah Bruner <jsbruner@umich.edu>
2022-04-14 17:00:52 -0400

    Fix some nvm issues

--- macos-setup.sh
+++ macos-setup.sh
@@ -18,7 +18,13 @@ brew install thefuck
 
 # Install Node / NPM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install node
+nvm use node
 
 # Setup vim
 curl https://raw.githubusercontent.com/JosiahOne/vimrc/master/.vimrc > ~/.vimrc
